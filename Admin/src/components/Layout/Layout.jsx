9397821e44b356c72c2e7164c5fb5b6cdd4f6d37
André Alessi <andrealessi89@gmail.com>
2024-02-21 01:28:45 -0300

    novo menu

--- Admin/src/components/Layout/Layout.jsx
+++ Admin/src/components/Layout/Layout.jsx
@@ -1,153 +1,143 @@
 import * as React from 'react';
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
+import MuiDrawer from '@mui/material/Drawer';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import { Button } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { Outlet } from 'react-router';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 import Menu from './Menu';
-import { useNavigate } from 'react-router';
-import useMediaQuery from '@mui/material/useMediaQuery';
-
-import Grid from '@mui/material/Grid'; // Grid version 1
-
-
-
-
+import { Outlet } from 'react-router';
 
 const drawerWidth = 240;
 
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
 
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-        flexGrow: 1,
-        padding: theme.spacing(3),
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: `-${drawerWidth}px`,
-        ...(open && {
-            transition: theme.transitions.create('margin', {
-                easing: theme.transitions.easing.easeOut,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-            marginLeft: 0,
-        }),
-    }),
-);
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
 
 const AppBar = styled(MuiAppBar, {
     shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-    transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
+  })(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
     }),
     ...(open && {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: `${drawerWidth}px`,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
     }),
-}));
+    backgroundColor: '#4a148c', // Exemplo: cor roxa. Substitua '#4a148c' pela cor desejada.
+  }));
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-}));
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
 
+export default function MiniDrawer() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-export default function Layout({ children }) {
-    const breadcrumbNameMap = {
-        'teste-1': 'teste1',
-        'Players': 'Players',
-    };
-    const pathnames = location.pathname.split('/').filter((x) => x);
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-    const [open, setOpen] = React.useState(true);
-    const navigate = useNavigate();
-    
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
-
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-        navigate('/login');
-    }
-    const drawerVariant = isMobile ? 'temporary' : 'persistent';
-
-    return (
-        <Box sx={{ display: 'flex', flexGrow: 1 }}>
-            <CssBaseline />
-            <AppBar position="fixed" open={open}>
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        sx={{ mr: 2, ...(open && { display: 'none' }) }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-                        Bem-vindo à aplicação
-                    </Typography>
-                    <Button color="inherit" onClick={handleLogout}>Logout</Button>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': {
-                        width: drawerWidth,
-                        boxSizing: 'border-box',
-                    },
-                }}
-                variant={drawerVariant}
-                anchor="left"
-                open={open}
-                onClose={handleDrawerClose} // Permite fechar clicando fora ou pressionando ESC em telas pequenas
-            >
-                <DrawerHeader>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                    </IconButton>
-                </DrawerHeader>
-                <Divider />
-                <Menu />
-            </Drawer>
-            <Main open={open}>
-                <DrawerHeader />
-                <Outlet />
-            </Main>
-        </Box>
-    );
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Maestro Horiochi
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <Menu open={open} />
+        <Divider />
+        
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 }
--- Admin/src/components/RetratoColorido/RetratoColoridoView.jsx
+++ Admin/src/components/RetratoColorido/RetratoColoridoView.jsx
@@ -4,6 +4,47 @@ import jwtDecode from "jwt-decode";
 import { toast } from 'react-toastify';
 import { differenceInSeconds, addHours } from 'date-fns'; // Importando funções do date-fns
 import { Link } from 'react-router-dom'; // Adicione isto se você estiver usando react-router
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
+import { Button } from '@mui/material';
+
+
+
+
+const cartaStyle = {
+  padding: '20px',
+  backgroundColor: '#f4f1e9', // Cor de fundo para parecer papel antigo
+  color: '#5a4a42', // Cor da fonte para imitar tinta
+  fontFamily: '"Times New Roman", Times, serif', // Fonte para parecer escrita à mão ou máquina de escrever
+  fontStyle: 'italic', // Itálico para dar um toque elegante
+  fontSize: '16px', // Tamanho da fonte
+  lineHeight: '1.6', // Espaçamento entre linhas
+  boxShadow: '5px 5px 10px rgba(0,0,0,0.1)', // Sombra suave para dar um efeito elevado
+  //margin: '20px', // Margem para separar do resto do conteúdo
+};
+
+const countdownStyle = {
+  fontSize: '24px',
+  fontWeight: 'bold',
+  textAlign: 'center',
+  marginBottom: '20px',
+};
+
+const buttonStyle = {
+  display: 'block',
+  width: 'max-content',
+  margin: '0 auto', // Centraliza o botão
+  padding: '10px 20px',
+  backgroundColor: '#1976d2',
+  color: '#fff',
+  textDecoration: 'none',
+  borderRadius: '4px',
+  textAlign: 'center',
+};
+
+const urlHoriochoi = 'fotos/horiochi.png';
 
 const RetratoColoridoView = () => {
   const [loading, setLoading] = useState(true);
@@ -11,6 +52,8 @@ const RetratoColoridoView = () => {
   const [countdown, setCountdown] = useState('');
   const [errorMessage, setErrorMessage] = useState(null);
   const [urlImg, setUrlImg] = useState(null);
+
+  const urlBanner = 'fotos/btnInformacoes.jpg';
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -38,7 +81,35 @@ const RetratoColoridoView = () => {
           }
 
           if (retrato_colorido !== 1) {
-            setErrorMessage(<div>Você precisa comprar esse produto.</div>);
+            setErrorMessage(
+              <Grid 
+                container 
+                spacing={2} 
+                direction="column" 
+                alignItems="center" 
+                justifyContent="center" // Centraliza verticalmente
+                style={{ minHeight: '100vh' }} // Altura mínima para ocupar a tela inteira
+              >
+                <img src={urlHoriochoi} height={150}></img>
+                <Grid item>
+                
+                  <Typography variant="body1" style={{ textAlign: 'center' }}>
+                    Você precisa comprar esse produto.
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Button 
+                    variant="contained" 
+                    color="primary" 
+                    onClick={() => window.location.href = 'https://www.exemplo.com/pagina-de-compra'}
+                  >
+                    Comprar Retrato Colorido
+                  </Button>
+                </Grid>
+              </Grid>
+            );
+            
+            
             return;
           }
 
@@ -100,13 +171,68 @@ const RetratoColoridoView = () => {
   }
 
   if (!isAvailable) {
-    return <div>Aguarde {countdown} para ver seu retrato.</div>;
+    return (
+      <Grid
+      container
+      spacing={2}
+      direction="column"
+      alignItems="center"
+      justifyContent="center" // Centraliza verticalmente
+      style={{ minHeight: '80vh' }} // Altura mínima para ocupar a tela inteira
+    >
+      <img src={urlHoriochoi} height={150}></img>
+      <Grid item>
+
+        <Typography variant="body1" style={{ textAlign: 'center' }}>
+        <Typography style={countdownStyle}>Aguarde {countdown} para ver seu retrato.</Typography>
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => window.location.href = 'https://www.exemplo.com/pagina-de-compra'}
+        >
+          Comprar envio imediato
+        </Button>
+      </Grid>
+    </Grid>
+  );
+
   }
 
   return (
-    <div>
-      <img src={urlImg} alt="Descrição da Imagem" />
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        {/* Banner */}
+        
+        {/* Imagem à esquerda */}
+        <Grid item xs={12} lg={6}>
+          <img src={urlImg} alt="Descrição da Imagem" style={{ width: '100%', height: 'auto' }} />
+        </Grid>
+        {/* Texto em forma de carta à direita */}
+        <Grid item xs={12} lg={6}>
+        <Grid item xs={12}>
+          <img src={urlBanner} alt="Descrição do Banner" style={{ width: '100%', height: 'auto' }} />
+        </Grid>
+          <Paper style={cartaStyle}>
+            <Typography variant="body1" component="p">
+              <b>Estimado ser de luz,</b>
+              <br /><br />
+              En este sagrado intercambio, donde el arte se encuentra con el alma, tu confianza en mi visión es el mayor honor. Al adquirir la imagen de tu alma gemela, has dado un paso valiente hacia el encuentro con tu esencia divina y tu conexión cósmica.
+              <br /><br />
+              Que este retrato sea un reflejo de la belleza infinita que reside en ti, y que su presencia en tu vida sirva como un constante recordatorio de la unidad y el amor que el universo tiene reservado para ti. Esta no es simplemente una transacción; es una afirmación de fe, un acto de reconocimiento de que las fuerzas mayores nos guían hacia nuestros destinos entrelazados.
+              <br /><br />
+              Con profunda gratitud y respeto, celebro tu camino hacia la iluminación y el amor verdadero. Que la imagen de tu alma gemela ilumine tu hogar con la misma luz con la que iluminaste mi camino al elegir mi obra.
+              <br /><br />
+              Con amor y luz,
+              <br />
+              <b>Maestro Horiochi</b>
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
   );
   
 };
