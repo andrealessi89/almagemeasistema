a2e2b9583e200750b6cf89411d78c3e3f2a5611e
André Alessi <andrealessi89@gmail.com>
2024-02-22 00:49:33 -0300

    ajuste conexao com db

--- Api/src/models/connection.js
+++ Api/src/models/connection.js
@@ -1,21 +1,23 @@
 const mysql = require('mysql2/promise');
 
+let pool; // Variável global para armazenar a pool de conexão
 
 const createConnectionPool = (database) => {
-    return mysql.createPool({
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: database,
-        port: process.env.MYSQL_PORT,
-        waitForConnections: true,
-        connectionLimit: 10000, // Ajuste conforme necessário
-        queueLimit: 0
-    });
+    if (!pool) { // Verifica se a pool ainda não foi criada
+        pool = mysql.createPool({
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: database,
+            port: process.env.MYSQL_PORT,
+            waitForConnections: true,
+            connectionLimit: 10000, // Ajuste conforme necessário
+            queueLimit: 0
+        });
+    }
+    return pool;
 };
-
 
 module.exports = {
     createConnectionPool
 };
-
